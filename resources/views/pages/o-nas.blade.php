606fa211a2651a9ccffdd90074faf28443387191
Michal Nosavcov <mnosavcov@gmail.com>
2018-11-14 18:22:56 +0100

    onas

--- resources/views/pages/o-nas.blade.php
+++ resources/views/pages/o-nas.blade.php
@@ -51,9 +51,9 @@
 
         <div class="row">
             <div class="col-12 col-xl-10 offset-xl-1 jak-to-zacalo">
-                Michala weby baví už dvacet let a po letech zkušeností se rozhodl pro svůj vlastní projekt, do kterého
-                přibral grafika Ondru a oslovil Jitku pro marketing.<br>Slovo dalo slovo a v poměrně krátké době vznikl
-                nový funkční tým a projekt "web nás baví" dostal reálné&nbsp;základy.
+                Michala weby baví už dvacet let a&nbsp;po letech zkušeností se rozhodl pro svůj vlastní projekt, do kterého
+                přibral grafika Ondru a&nbsp;oslovil Jitku pro marketing.<br>Slovo dalo slovo a&nbsp;v&nbsp;poměrně krátké době vznikl
+                nový funkční tým a&nbsp;projekt "web&nbsp;nás&nbsp;baví" dostal reálné&nbsp;základy.
             </div>
         </div>
     </div>
